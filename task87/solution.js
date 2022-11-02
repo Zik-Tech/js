179958fcda0488a206a4fb80f1f3b8fb62509082
Zik-Tech <adioisaac24@gmail.com>
2022-11-02 21:53:22 +0100

    Task completed

---
+++ task87/solution.js
@@ -0,0 +1,10 @@
+/**
+ * Create 5 constants.
+ *
+ * Choose any names you like.
+ * */
+const firstName = null;
+const secondName = null;
+const age = null;
+const sex = null;
+const bestFriend = null;
---
+++ task90/solution.js
@@ -0,0 +1,6 @@
+import {API_URL, MAX_THREADS} from './constants.js'
+
+console.log(MAX_THREADS);
+
+console.log(API_URL)
+console.log(MAX_THREADS)

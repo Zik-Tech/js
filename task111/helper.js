d4a3e618823bdb1d30251460f4273b583fe99f2d
Zik-Tech <adioisaac24@gmail.com>
2022-11-07 00:33:07 +0100

    Todays task completed

---
+++ task111/helper.js
@@ -0,0 +1,6 @@
+export const user = {
+  age: '100',
+  name: "izik",
+  surname: "Tech",
+  address: "Nigeria",
+};
---
+++ task115/helper.js
@@ -0,0 +1,5 @@
+export const objects = [
+    {"object1": 1},
+    {"object2": 2},
+    {"object3": 3},
+];
